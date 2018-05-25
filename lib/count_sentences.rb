9fd416ba22fd6d66076f249124d969ad78d54e5f
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-25 22:17:44 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.split(".?!")
+    if self.length > 0
+      self.length
+      binding.pry
+    elsif self.length == nil
+      "0"
+    end
   end
 end
